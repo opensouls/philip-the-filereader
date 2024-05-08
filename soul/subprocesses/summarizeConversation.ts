24690f7034c554dc7b9b048249f0b9933943e1b4
Topper Bowers <topper@toppingdesign.com>
2024-05-08 16:08:15 +0200

    turn off the summarizer

--- soul/subprocesses/summarizeConversation.ts
+++ soul/subprocesses/summarizeConversation.ts
@@ -2,7 +2,8 @@ import { MentalProcess } from "@opensouls/engine";
 import summarizesConversation from "../cognitiveFunctions/summarizeConversation.js";
 
 const summarizeProcess: MentalProcess = async ({ workingMemory }) => {
-  return summarizesConversation({ workingMemory })
+  return workingMemory
+  // return summarizesConversation({ workingMemory })
 }
 
 export default summarizeProcess
