cd496b9f21e6816012f6b9f65e8a0c7b4aee00dd
Topper Bowers <topper@toppingdesign.com>
2024-05-10 09:20:04 +0200

    spacing

--- soul/mentalProcesses/readsAFile.ts
+++ soul/mentalProcesses/readsAFile.ts
@@ -15,7 +15,6 @@ const tools: ToolPossibilities = {
   "edit": {
     description: indentNicely`
       Edit a section of the open file. Philip loves this tool, it's fun.
-
       Philip provides the start and end lines to the edit, and any commentary on why he wants to edit.
     `,
     params: z.object({
